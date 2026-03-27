f53cbfc93de0adcfe693bb5991e7233055b1c9e0
harishvskv <hari.vskv@gmail.com>
2026-03-27 14:07:09 +0530

    Update Controller_DSC_sEC_HOCBF.m

--- Controller_DSC_sEC_HOCBF.m
+++ Controller_DSC_sEC_HOCBF.m
@@ -1,26 +1,26 @@
-function U_edge = Controller_DSC_sEC_HOCBF(x, d, params)
+function U_edge = Controller_DSC_SEC_HOCBF(x, d, params)
     % Solves the High-Order CBF Quadratic Program for Edge Control
     
-    dw = x(1); Pm = x(2); Pv = x(3); PB = x(4); PAC = x(7);
+    df = x(1); Pm = x(2); Pv = x(3); PB = x(4); PAC = x(7);
     P_res = d(1); P_L0 = d(2);
 
     %% 1. Nonlinear Relative Degree Formulation
-    dw_bound = max(dw, -0.5);
-    P_load = P_L0 * (1 + params.D * dw_bound)^params.gamma;
-    f1 = (1/params.M) * (Pm + P_res + PB - PAC - P_load); % dw_dot
+    df_bound = max(df, -0.5);
+    P_load = P_L0 * (1 + params.D * df_bound)^params.gamma;
+    f1 = (1/params.M) * (Pm + P_res + PB - PAC - P_load); % df_dot
 
     Pm_dot = (-Pm + Pv) / params.Tt;
     PB_dot = -(1/params.TB) * PB;
     PAC_dot = -(1/params.Tac) * PAC;
-    Pload_dot = params.D * params.gamma * P_L0 * (1 + params.D * dw_bound)^(params.gamma - 1) * f1;
+    Pload_dot = params.D * params.gamma * P_L0 * (1 + params.D * df_bound)^(params.gamma - 1) * f1;
 
     % Lie Derivatives
     Lf2h = (1/params.M) * (Pm_dot + PB_dot - PAC_dot - Pload_dot);
     LgLfh = [1/(params.M * params.TB), -1/(params.M * params.Tac)];
 
     %% 2. Barrier Definition
-    dw_min = -0.02; % Safety Limit (pu)
-    h = dw - dw_min;
+    df_min = -0.02; % Safety Limit (pu) fBse = 50Hz
+    h = df - df_min;
     
     % Class-K parameter tuning
     cb1 = 25; cb2 = 25;
@@ -29,8 +29,8 @@ function U_edge = Controller_DSC_sEC_HOCBF(x, d, params)
     B_cbf = Lf2h + (cb1 + cb2) * f1 + cb1 * cb2 * h;
 
     %% 3. Quadratic Program Formulation
-    % Optimize BESS and AIAC dispatch while penalizing thermodynamic deviation
-    H = diag([10, 500]); % Heavily penalize AIAC usage to preserve comfort
+    % Optimize BESS and AIAC dispatch while penalizing thermodynamic deviation (for Thermal Comfort)
+    H = diag([10, 500]); % Penalize AIAC usage to preserve comfort
     f_qp = [0; 0];
     
     % Physical Actuator Limits
