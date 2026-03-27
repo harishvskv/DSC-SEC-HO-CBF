927a60f51c1dcf73185d0ec46ebf48010bc037ea
harishvskv <hari.vskv@gmail.com>
2026-03-27 14:13:46 +0530

    Update Main_Nonlinear_Microgrid_DoS.m

--- Main_Nonlinear_Microgrid_DoS.m
+++ Main_Nonlinear_Microgrid_DoS.m
@@ -1,21 +1,32 @@
 %% Main_Nonlinear_Microgrid_DoS.m
 % Simulates the Cyber-Physical Microgrid under DoS Attack
-% Demonstrates the transition from vulnerable WAN-PI control to DSC-sEC.
+% Demonstrates the transition from vulnerable Conventional-PI control to DSC-sEC.
 
 % 1. Load Nonlinear System Parameters
-params.M = 8.0; params.D = 1.2; params.gamma = 1.5; % Nonlinear load exponent
-params.Tt = 0.3; params.Tg = 0.2; params.R = 0.04;
-params.TB = 0.1; params.Tac = 0.2; 
-params.C_bat = 500; params.R_th = 2; params.C_th = 8; params.COP = 2.5; 
+params.M = input ('Enter Value, M:'); params.D = input ('Enter Value, D:'); params.gamma = input ('Enter Value, gamma:'); % Nonlinear load exponent
+params.Tt = input ('Enter Value, Tt:'); params.Tg = input ('Enter Value, Tg:'); params.R = input ('Enter Value, R in p.u.:');
+params.TB = input ('Enter Value, TB:'); params.Tac = input ('Enter Value, Tac:'); 
+params.C_bat = input ('Enter Value, C_BESS:'); params.R_th = input ('Enter Value, RTh:'); params.C_th = input ('Enter Value, CTh:'); params.COP = input ('Enter Value, CoP:'); 
 
 % 2. Simulation Setup
-Tsim = 200; Ts = 0.001; t = 0:Ts:Tsim; N = length(t);
-X_baseline = zeros(8, N); % [dw, Pm, Pv, PB, SoC, Tin, PAC, int_dw]
+Tsim = input ('Enter Value:'); Ts = 0.1e-3; t = 0:Ts:Tsim; N = length(t);
+X_baseline = zeros(8, N); % [df, Pm, Pv, PB, SoC, Tin, PAC, int_df]
 X_proposed = zeros(8, N);
 
 % 3. DoS Attack Definition (Breaching SC-ATD bound)
-t_attack_start = 115.0; 
-t_attack_end = 118.0; 
+%t_attack_start = 115.0; %Deterministic case 
+%t_attack_end = 118.0; 
+
+% Stochastic Case
+alpha = ones(1,N);
+% Number of attacks (Poisson-like)
+num_attacks = randi([1,3]);  
+for k_attack = 1:num_attacks
+    t_start = 80 + 120*rand();        % anywhere in simulation
+    %duration = 0.3 + 3*rand();        % random duration
+    duration = 2.0 + 1.4 * rand();
+    alpha(t >= t_start & t <= t_start + duration) = 0;
+end
 
 % 4. Main Integration Loop
 for k = 1:N-1
