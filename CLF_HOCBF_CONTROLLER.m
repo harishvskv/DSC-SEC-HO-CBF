ea2ec8853f8a134f116244194bc6308701385a15
harishvskv <hari.vskv@gmail.com>
2026-03-27 14:05:38 +0530

    Refactor CLF_HOCBF_CONTROLLER for variable naming

--- CLF_HOCBF_CONTROLLER.m
+++ CLF_HOCBF_CONTROLLER.m
@@ -1,35 +1,35 @@
 function [u1, u2] = CLF_HOCBF_CONTROLLER(x, d, params)
-    dw = x(1); Pm = x(2); Pv = x(3); PB = x(4); S = x(5); Tin = x(6); PAC = x(7); int_dw = x(8);
+    df = x(1); Pm = x(2); Pv = x(3); PB = x(4); S = x(5); Tin = x(6); PAC = x(7); int_df = x(8);
     Pres = d(1); P_L0 = d(2);
 
     %% 1. Grid Dynamics & Relative Degree 2 Variables
-    dw_bound = max(dw, -0.5);
-    P_load = P_L0 * (1 + params.D * dw_bound)^params.gamma;
+    df_bound = max(dw, -0.5);
+    P_load = P_L0 * (1 + params.D * df_bound)^params.gamma;
 
-    f1 = (1/params.M) * (Pm + Pres + PB - PAC - P_load); % dw_dot
+    f1 = (1/params.M) * (Pm + Pres + PB - PAC - P_load); % df_dot
     Pm_dot = (-Pm + Pv) / params.Tt;
     PB_dot = -(1/params.TB) * PB;
     PAC_dot = -(1/params.Tac) * PAC;
-    Pload_dot = params.D * params.gamma * P_L0 * (1 + params.D * dw_bound)^(params.gamma - 1) * f1;
+    Pload_dot = params.D * params.gamma * P_L0 * (1 + params.D * df_bound)^(params.gamma - 1) * f1;
 
     Lf2h = (1/params.M) * (Pm_dot + PB_dot - PAC_dot - Pload_dot);
     LgLfh = [1/(params.M * params.TB), -1/(params.M * params.Tac)];
 
-    %% 2. Reference Controller (PI + Virtual Inertia)
-    Kp = 1.5; Ki = 0.8; K_vi = 0.6;
-    u1_ref = -(Kp * dw + Ki * int_dw + K_vi * f1); 
-    u2_ref = 0.2 + (Kp * dw + Ki * int_dw); 
+    %% 2. Reference Controller (PI + Virtual Inertia->BESS)
+    Kp = 1.5; Ki = 0.8; K_vi = 0.6;  %From Literature
+    u1_ref = -(Kp * dw + Ki * int_df + K_vi * f1); 
+    u2_ref = 0.2 + (Kp * dw + Ki * int_df); 
 
     %% 3. HOCBF (Safety)
-    dw_min = -0.02;
-    h = dw - dw_min;
-    cb1 = 8; cb2 = 8;
+    df_min = -0.02;  %fBase = 50Hz
+    h = df - df_min;
+    cb1 = 8; cb2 = 8; %Tuned
     A_cbf = -LgLfh;
     B_cbf = Lf2h + (cb1 + cb2) * f1 + cb1 * cb2 * h;
 
-    %% 4. Backstepping CLF (Stability)
+    %% 4. Backstepping CLF (Stability Analysis)
     c1 = 4; c2 = 4;
-    z1 = dw; z2 = f1 + c1 * z1;
+    z1 = df; z2 = f1 + c1 * z1;
     V = 0.5 * z1^2 + 0.5 * z2^2;
     A_clf = [z2 * LgLfh(1), z2 * LgLfh(2), -1];
     B_clf = -c2 * V + c1 * z1^2 - z1 * z2 - z2 * (Lf2h + c1 * f1);
