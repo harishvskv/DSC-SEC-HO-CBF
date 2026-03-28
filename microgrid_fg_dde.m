659e1db0d7ef867a599f3f2cc36d42a0dfd9ffbf
harishvskv <hari.vskv@gmail.com>
2026-03-28 16:30:14 +0530

    Update microgrid_fg_dde.m

--- microgrid_fg_dde.m
+++ microgrid_fg_dde.m
@@ -6,26 +6,49 @@ function [f,g] = microgrid_fg_dde(x, d, params, UFLS_flag)
     Pres = d(1); 
     P_L0 = d(2);
     
-    % Apply UFLS if triggered (shed 18% of base load)
+    % Apply UFLS if triggered (shed 10% of base load)
     if UFLS_flag
-        P_L0 = P_L0 - 0.18;
+        P_L0 = P_L0 - 0.10;
     end
     
     f = zeros(8,1);
     g = zeros(8,2);
     
-    df_bound = max(dw, -0.5);
+    % Fixed typo: changed dw to df
+    df_bound = max(df, -0.5);
     P_load = P_L0 * (1 + params.D * df_bound)^params.gamma;
     
     %% Swing Equation
     f(1) = (1/params.M) * (Pm + Pres + PB - PAC - P_load);
     
-    %% Turbine
-    f(2) = (-Pm + Pv) / params.Tt;
+       
+    %% Governor Dead-Band (GDB) Filter
+    GDB_range = 0.0006;           % ±0.0006 Hz dead band (NERC)
     
+    if abs(df) <= GDB_range
+        df_gdb = 0;               % Inside dead-band: no governor action
+    elseif df > GDB_range
+        df_gdb = df - GDB_range;  % Positive deviation beyond dead-band
+    else
+        df_gdb = df + GDB_range;  % Negative deviation beyond dead-band
+    end
+
     %% Governor (Primary + AGC interaction)
     Pref = params.P_ref; 
-    f(3) = (Pref - Pv - (1/params.R)*df) / params.Tg;
+    % Author Tried using df_gdb instead of df so the governor ignores tiny 
+variations
+    f(3) = (Pref - Pv - (1/params.R)*df_gdb) / params.Tg;
+    
+    %% Generation Rate Constraint (GRC) & Turbine
+    GRC_limit = 0.0017;           % pu/s generation rate constraint
+    
+    % 1. unbounded rate of change of mechanical power
+    dPm_dt_unconstrained = (-Pm + Pv) / params.Tt;
+    
+    % 2. Clamp the rate of change using max/min limits
+    f(2) = max(-GRC_limit, min(GRC_limit, dPm_dt_unconstrained));
+
+    % =========================================================
     
     %% Battery Power
     f(4) = -(1/params.TB) * PB;
